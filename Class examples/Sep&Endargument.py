9e7c0719790f590b3aa6f2d030869d36758a4f5e
melkay14 <magdelkayfe@gmail.com>
2025-03-02 05:37:41 +0000

    sep&end

---
+++ Class examples/Sep&Endargument.py
@@ -0,0 +1,12 @@
+# sep adds something after one quote ends 
+
+print("HelloWorld", "!!", sep="^^")
+
+#end arguemnt helps put print lines together
+
+print("Hello", end=" ")
+print("World", end="")
+print("!!")
+
+for i in range(11):
+    print(i, end=",")
