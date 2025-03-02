165675d117b6f5082e6d55be23351042ae3979d5
melkay14 <magdelkayfe@gmail.com>
2025-03-02 05:37:04 +0000

    inputconversions

---
+++ Class examples/inputconversions.py
@@ -0,0 +1,8 @@
+letGrade = input("Enter your grade(A-F): ")
+numGrade = int(input("Enter your grade(0-100): " ) )
+average =  float(input("Enter your average(0.0- 100.0): " ) )
+gpa = eval(input("enter your GPA(0-4): " ) )
+
+print("Grade", numGrade+40)
+print("Average:", average/2)
+print("GPA:", gpa)
