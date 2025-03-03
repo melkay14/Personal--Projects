a642158cecec33210e0fea952ba6ea16088fd9c3
melkay14 <magdelkayfe@gmail.com>
2025-03-03 17:49:03 +0000

    justifying output

---
+++ Class examples/justifyingoutput.py
@@ -0,0 +1,7 @@
+# ljust rjust center()
+
+print("01234567890122345678901234567")
+print("Rank".ljust(5), "Player".rjust(21),"Hr".rjust(3), sep="")
+print('1'.center(5), "Barry Bonds".ljust(20), "762".rjust(3), sep="")
+print('2'.center(5), "Hank Aaron".ljust(20), "755".rjust(3), sep="")
+print('3'.center(5), "Babe Ruth".ljust(20), "714".rjust(3), sep="")
